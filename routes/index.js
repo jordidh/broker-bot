fb964458baa33eefe8f64b61ce330e9771f5e5d8
jordidh <jordi.dh@gmail.com>
2021-03-09 15:32:42 +0100

    feat:nova pantalla de grafiques

--- routes/index.js
+++ routes/index.js
@@ -31,4 +31,20 @@ router.get('/', function (req, res, next) {
     }
 });
 
+router.get('/dashboard', function (req, res, next) {
+    let postId = uuidv1();
+
+    // Enviem missatge al telegram de l'usuari per indicar que hem rebut un post
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+    logger.info(postId + `: Rebut GET des de ` + ip);
+
+    try {
+        res.render('dashboard', { name: pjson.name, version: pjson.version, views: config.dashboard.views });
+    } catch (e) {
+        logger.error(e);
+        res.render('dashboard', { name: pjson.name, version: pjson.version, m_alert: e.message });
+    }
+});
+
 module.exports = router;
